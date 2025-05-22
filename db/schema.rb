fa5dd93222d2e7c6bfd726f234e5a31e83c6cab3
develumpen <hi@develumpen.com>
2025-05-21 19:11:53 -0500

    add entry model & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_21_235134) do
+  create_table "entries", force: :cascade do |t|
+    t.datetime "published_at", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
